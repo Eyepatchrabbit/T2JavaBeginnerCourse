0cc806c8069f0316406e28584ea3db283e70107b
Alexander B <a2.boffin@gmail.com>
2019-04-05 22:22:19 +0200

    CodeCata01 - first try for solving a codecata
    -initial setup

---
+++ src/main/java/codekata/_Readme.txt
@@ -0,0 +1,6 @@
+This location is for coding exercises.
+
+Places where exrecises can be found:
+    * https://projecteuler.net
+    * http://codekata.com
+    * https://www.codewars.com
---
+++ src/main/java/codekata/wars/DescendingOrder.java
@@ -0,0 +1,16 @@
+package codekata.wars;
+
+public class DescendingOrder {
+    //Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+    //Examples:
+    //Input: 21445 Output: 54421
+
+    //Input: 145263 Output: 654321
+
+    //Input: 1254859723 Output: 9875543221
+
+    public static int sortDesc(final int num) {
+        //Your code
+        return -1;
+    }
+}
