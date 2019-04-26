7676e3b6a3855da647c56721ea4c6e37918174ec
Alexander B <boffin.alexander@gmail.com>
2019-04-26 21:22:39 +0200

    HomeAssignment - bmi calculator

--- src/main/java/beginnercoursettwo/les02/homework/BmiCalculation.java
+++ src/main/java/beginnercoursettwo/les02/homework/BmiCalculation.java
@@ -1,5 +1,49 @@
 package beginnercoursettwo.les02.homework;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class BmiCalculation {
     //make a app to calculate your own BMI
+
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        try{
+            System.out.print("geef gewicht in kg: ");
+            Double bodyWeight= scanner.nextDouble();
+
+            System.out.print("geef je length in m: ");
+            Double length= scanner.nextDouble();
+
+            Double bmi= bmiCalculator (bodyWeight,length);
+
+            System.out.println("Je BMI is : "+bmi);
+
+            System.out.println("je hebt dus " + bmiIndex(bmi));
+        }finally {
+            scanner.close();
+        }
+
+    }
+
+
+    private static Double bmiCalculator(Double bodyWeight, Double length){
+        return  bodyWeight/(length*length);
+    }
+
+
+
+    private static String bmiIndex(Double bmi){
+        if(bmi>30.0){
+            return "Obesitas";
+        } else if (bmi>=25.0){
+            return "overgewicht";
+        }else if (bmi>=18.5){
+            return "gezond normaal gewicht";
+        }else if (bmi>0.0){
+            return "ondergewicht";
+        }
+        return "onbekend";
+    }
+
 }
