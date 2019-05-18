8b094bc6010f292d95c6fae74bfc0ced5c3e9e2d
Alexander B <boffin.alexander@gmail.com>
2019-05-18 13:58:42 +0200

    CodeCata01 - tests decendingOrder
    -activate tests of decendingOrder testcases

--- src/test/java/codekata/wars/DescendingOrderTest.java
+++ src/test/java/codekata/wars/DescendingOrderTest.java
@@ -7,21 +7,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DescendingOrderTest {
 
-    @Ignore
+
     @Test
     public void test_01() {
         assertEquals(0, DescendingOrder.sortDesc(0));
     }
 
-    @Ignore
+
     @Test
     public void test_02() {
         assertEquals(51, DescendingOrder.sortDesc(15));
     }
 
-    @Ignore
+
     @Test
     public void test_03() {
         assertEquals(987654321, DescendingOrder.sortDesc(123456789));
+    }
+
+
+    @Test
+    public void test_04() {
+        assertEquals(2110, DescendingOrder.sortDesc(1021));
     }
 }
